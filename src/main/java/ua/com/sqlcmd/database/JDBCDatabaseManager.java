9134ef14fad31d63477ed4f4c4453cd3302408fa
Pavlo Vyshniakov <pashki4@gmail.com>
2022-08-17 21:08:36 +0300

    refactoring

--- src/main/java/ua/com/sqlcmd/database/JDBCDatabaseManager.java
+++ src/main/java/ua/com/sqlcmd/database/JDBCDatabaseManager.java
@@ -8,7 +8,7 @@ public class JDBCDatabaseManager implements DatabaseManager {
     private static final String NEW_LINE = System.lineSeparator();
     private static final String QUESTION_MARK = "?";
     private static final String DATABASE_URL = "jdbc:postgresql://127.0.0.1:5432/";
-    private static final String SQL_SELECT_TABLE_NAMES_SQL = "SELECT table_name" + NEW_LINE
+    private static final String SELECT_TABLE_NAMES_SQL = "SELECT table_name" + NEW_LINE
             + " FROM information_schema.tables" + NEW_LINE
             + " WHERE table_schema='public'" + NEW_LINE
             + " AND table_type='BASE TABLE';";
@@ -28,8 +28,8 @@ public class JDBCDatabaseManager implements DatabaseManager {
             this.password = password;
             isConnected = true;
         } catch (SQLException e) {
-            throw new RuntimeException("Не можу отримати з'єднання з такими параметрами: %n"
-                    + "база: %s, юзер: %s, пароль: %s%n".formatted(database, userName, password), e);
+            throw new RuntimeException("Не можу підключитися з такими параметрами: %n"
+                    + "database: %s, user: %s, password: %s%n".formatted(database, userName, password), e);
         }
     }
 
@@ -37,8 +37,8 @@ public class JDBCDatabaseManager implements DatabaseManager {
     public Set<String> getTables() {
         try (Connection connection = DriverManager
                 .getConnection(DATABASE_URL + database, userName, password)) {
-            PreparedStatement preparedStatement = connection.prepareStatement(SQL_SELECT_TABLE_NAMES_SQL);
-            ResultSet resultSet = preparedStatement.executeQuery();
+            PreparedStatement selectTableNamesStatement = connection.prepareStatement(SELECT_TABLE_NAMES_SQL);
+            ResultSet resultSet = selectTableNamesStatement.executeQuery();
             Set<String> result = new LinkedHashSet<>();
             while (resultSet.next()) {
                 result.add(resultSet.getString("table_name"));
@@ -68,6 +68,29 @@ public class JDBCDatabaseManager implements DatabaseManager {
         }
     }
 
+    private int getRowCount(String tableName) {
+        try (Connection connection =
+                     DriverManager.getConnection(DATABASE_URL + database, userName, password)) {
+            PreparedStatement preparedStatementSelectRowCount = connection.prepareStatement(SELECT_ALL_COUNT_SQL + tableName);
+            ResultSet resultSet = preparedStatementSelectRowCount.executeQuery();
+            resultSet.next();
+            int columnIndex = 1;
+            return resultSet.getInt(columnIndex);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private DataSet createEmptyDataSetWithNames(ResultSet resultSet) throws SQLException {
+        Set<String> columnNames = getColumnNames(resultSet);
+
+        DataSet temp = new DataSetImpl();
+        for (String columnName : columnNames) {
+            temp.put(columnName, "");
+        }
+        return temp;
+    }
+
     private static List<DataSet> getAllData(ResultSet resultSet, int rowCount) throws SQLException {
         List<DataSet> result = new ArrayList<>(rowCount);
         while (resultSet.next()) {
@@ -87,16 +110,6 @@ public class JDBCDatabaseManager implements DatabaseManager {
         return columnNamesWithEmptyValues;
     }
 
-    private DataSet createEmptyDataSetWithNames(ResultSet resultSet) throws SQLException {
-        Set<String> columnNames = getColumnNames(resultSet);
-
-        DataSet temp = new DataSetImpl();
-        for (String columnName : columnNames) {
-            temp.put(columnName, "");
-        }
-        return temp;
-    }
-
     private Set<String> getColumnNames(ResultSet resultSet) throws SQLException {
         int columnCount = resultSet.getMetaData().getColumnCount();
         Set<String> columnNames = new LinkedHashSet<>();
@@ -105,19 +118,6 @@ public class JDBCDatabaseManager implements DatabaseManager {
             columnNames.add(resultSet.getMetaData().getColumnName(index++));
         }
         return columnNames;
-    }
-
-    private int getRowCount(String tableName) {
-        try (Connection connection =
-                     DriverManager.getConnection(DATABASE_URL + database, userName, password)) {
-            PreparedStatement preparedStatementSelectRowCount = connection.prepareStatement(SELECT_ALL_COUNT_SQL + tableName);
-            ResultSet resultSet = preparedStatementSelectRowCount.executeQuery();
-            resultSet.next();
-            int columnIndex = 1;
-            return resultSet.getInt(columnIndex);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     @Override
@@ -143,14 +143,6 @@ public class JDBCDatabaseManager implements DatabaseManager {
         }
     }
 
-    private static void fillInsertStatementWithData(DataSet dataSet, PreparedStatement insertStatement) throws SQLException {
-        List<Object> input = dataSet.getValues();
-        int parameterIndex = 1;
-        for (int i = 0; i < dataSet.getValues().size(); i++) {
-            insertStatement.setObject(parameterIndex++, input.get(i));
-        }
-    }
-
     private PreparedStatement prepareInsertStatement(Connection connection, DataSet dataSet, String tableName) {
         try {
             String insertIntoSQL = collectSQLInsertQuery(dataSet, tableName);
@@ -160,18 +152,26 @@ public class JDBCDatabaseManager implements DatabaseManager {
         }
     }
 
+    private static void fillInsertStatementWithData(DataSet dataSet, PreparedStatement insertStatement) throws SQLException {
+        List<Object> input = dataSet.getValues();
+        int parameterIndex = 1;
+        for (int i = 0; i < dataSet.getValues().size(); i++) {
+            insertStatement.setObject(parameterIndex++, input.get(i));
+        }
+    }
+
     private static String collectSQLInsertQuery(DataSet dataSet, String tableName) {
-        String columnNames = prepareColumnNames(dataSet);
-        String values = prepareValues(dataSet);
+        String columnNames = collectColumnNames(dataSet);
+        String values = collectValues(dataSet);
         String sqlInsert = "INSERT INTO public." + tableName + " (" + columnNames + ") VALUES (" + values + ")";
         return sqlInsert;
     }
 
-    private static String prepareColumnNames(DataSet dataSet) {
+    private static String collectColumnNames(DataSet dataSet) {
         return String.join(", ", dataSet.getColumnNames());
     }
 
-    private static String prepareValues(DataSet dataSet) {
+    private static String collectValues(DataSet dataSet) {
         String[] questionMarkSeq = new String[dataSet.getValues().size()];
         Arrays.fill(questionMarkSeq, QUESTION_MARK);
         return String.join(", ", questionMarkSeq);
@@ -196,7 +196,7 @@ public class JDBCDatabaseManager implements DatabaseManager {
         try {
             return connection.prepareStatement(createSQL);
         } catch (SQLException e) {
-            throw new RuntimeException("Can't prepare statement to create table", e);
+            throw new RuntimeException("Не можу створити 'prepare statement to create table' ", e);
         }
     }
 
@@ -219,18 +219,18 @@ public class JDBCDatabaseManager implements DatabaseManager {
     }
 
     @Override
-    public void updateTableData(String tableName, String columnName, String columnValue, DataSet newValue) {
+    public void updateTableData(String tableName, String columnNameCriteria, String columnValueCriteria, DataSet newValue) {
         Set<String> columnNames = newValue.getColumnNames();
         var listNames = new ArrayList<>(columnNames);
         List<Object> values = newValue.getValues();
-        String formattedColumnNames = "";
+        String formattedColumnNames;
         if (columnNames.size() == 1) {
             formattedColumnNames = listNames.get(0) + " = ?";
         } else {
             formattedColumnNames = String.join(" = ?, ", columnNames) + " = ?";
         }
         String sqlUpdate = "UPDATE " + tableName + " SET " + formattedColumnNames +
-                " WHERE " + columnName + " = '" + columnValue + "'";
+                " WHERE " + columnNameCriteria + " = '" + columnValueCriteria + "'";
 
         try (Connection connection = DriverManager
                 .getConnection(DATABASE_URL + database, userName, password)) {
